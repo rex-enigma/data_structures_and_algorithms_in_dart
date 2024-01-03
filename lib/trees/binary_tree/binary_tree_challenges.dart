26787a837e94f4a0e8090ca31b1e2f32be68996f
josh <infinityjosh.jn@gmail.com>
2024-01-03 19:38:50 +0300

    data structures and algorithms in dart

--- lib/trees/binary_tree/binary_tree_challenges.dart
+++ lib/trees/binary_tree/binary_tree_challenges.dart
@@ -1,4 +1,8 @@
+import 'package:dart_data_structure_and_algorithm/trees/binary_tree/binary_tree.dart';
+
 // challenge 1: height of the tree
 // given a binary tree find the height of the tree. The height of a binary tree is determined by the distance
 // between the root and the furthest leaf. The height of a binary tree with a single node is  zero since
 // the single node is both the root and the furthest leaf.
+
+int treeHeight(BinaryNode root) {}

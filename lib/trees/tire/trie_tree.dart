ec4911a27135ad2a18f2842f2322b05ffebc2a4a
josh <infinityjosh.jn@gmail.com>
2024-01-23 18:47:07 +0300

    data structures and algorithms in dart

--- lib/trees/tire/trie_tree.dart
+++ lib/trees/tire/trie_tree.dart
@@ -1,4 +1,5 @@
 class TrieNode<T> {
+  TrieNode({this.keyPart, this.parent});
   // this keyPart represents a single character in a word or any character which when put together with other characters held in TireNodes forms a meaningful collection eg a word, ip address etc
   // is nullable because the TrieNode root doesn't have a keyPart
   T? keyPart;
@@ -11,4 +12,11 @@ class TrieNode<T> {
 
   // it marks the end of a collection( of a word,ip address etc)
   bool isTerminating = false;
+}
+
+class TrieTree {
+  // in dart a String is a collection of UTF-16 code units( each code unit{should be code point} is a number that represent a character). Therefore
+  // you can only iterate on the String characters in their numerical form (which in this case the string characters will be of int type). Each
+  // TireNode will hold a number that represent a character, thats why we are using int type for keyPart.
+  TrieNode<int> root = TrieNode(keyPart: null, parent: null);
 }

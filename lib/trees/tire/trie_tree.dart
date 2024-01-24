068a6679b472c483922f4a510ed25c9a2f1b3a01
josh <infinityjosh.jn@gmail.com>
2024-01-24 18:50:40 +0300

    data structures and algorithms in  dart

--- lib/trees/tire/trie_tree.dart
+++ lib/trees/tire/trie_tree.dart
@@ -1,11 +1,11 @@
 class TrieNode<T> {
   TrieNode({this.keyPart, this.parent});
   // this keyPart represents a single character in a word or any character which when put together with other characters held in TireNodes forms a meaningful collection eg a word, ip address etc
-  // is nullable because the TrieNode root doesn't have a keyPart
+  // is nullable because the TrieNode root doesn't have a keyPart.
   T? keyPart;
 
   // this will simplifies removing method.
-  //is nullable because the TireNode root doesn't have a parent.
+  // is nullable because the TireNode root doesn't have a parent.
   TrieNode<T>? parent;
 
   Map<T, TrieNode<T>> children = {};
@@ -19,4 +19,14 @@ class TrieTree {
   // you can only iterate on the String characters in their numerical form (which in this case the string characters will be of int type). Each
   // TireNode will hold a number that represent a character, thats why we are using int type for keyPart.
   TrieNode<int> root = TrieNode(keyPart: null, parent: null);
+
+  /// insert a word (collection of characters) in a trie tree.
+  void insert(String word) {
+    var current = root;
+    for (var codeUnit in word.codeUnits) {
+      current.children[codeUnit] ??= TrieNode(keyPart: codeUnit, parent: current);
+      current = current.children[codeUnit]!;
+    }
+    current.isTerminating = true;
+  }
 }

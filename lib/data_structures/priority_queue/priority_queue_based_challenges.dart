3479a401766b7e3dbc5327054d8d99ac1e01796e
josh <infinityjosh.jn@gmail.com>
2024-02-07 19:15:24 +0300

    data structures and algorithms in dart

--- lib/data_structures/priority_queue/priority_queue_based_challenges.dart
+++ lib/data_structures/priority_queue/priority_queue_based_challenges.dart
@@ -1,2 +1,12 @@
-// challenge 1: prioritize a wait list
-//  
+// challenge 1: prioritize a waitList
+// Your favorite concert was sold out. Fortunately there's is a waitList for people who still want to go.
+// However, ticket sales will first prioritize someone with military background, followed by seniority.
+// Use a priority queue to prioritize the order of people on the waitList. Start by making a Person class
+// that you can instantiate like so: final person = Person(name: 'Josh', age: 21, isMilitary: true);
+
+class Person {
+  String name;
+  int age;
+  bool isMilitary;
+  Person({required this.name, required this.age, required this.isMilitary});
+}
